5a9bb1a5a7642282f5dc06688cfa7fdcccf421d7
lanrelawal0022 <lanrelawal2002@gmail.com>
2022-07-04 21:30:37 +0000

    more files

---
+++ PYTHONGURU/Console.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+name = input("Enter your name right here: ")
+
+print(name)
---
+++ PYTHONGURU/Console2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+age = int(input("Enter your age here: "))
+
+print(age)
+print(type(age))
