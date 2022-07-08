244a526ea16265c35928a8906bdb9fbe0ea28e29
lanrelawal0022 <lanrelawal2002@gmail.com>
2022-07-08 02:53:36 -0700

    added files

---
+++ PYTHONGURU/section2/ord-chr.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+ch = 'b'
+
+print(ord(ch))
+
+print(chr(97))
+
+print(ord('A'))
+
+print("interlude...here")
+
+print(ord('D'))
+print(chr(105))
+print(ord('q'))
+print(chr(12))
--- PYTHONGURU/section2/stringops4.py
+++ PYTHONGURU/section2/stringops4.py
@@ -7,3 +7,6 @@ print(m[0:6])
 
 print(m[0:-7])
 print(m[:6])
+
+print(m[3:-5])
+print(m[3:8])
