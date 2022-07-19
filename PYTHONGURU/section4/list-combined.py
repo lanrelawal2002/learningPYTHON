68d4e0b3542d1a5aef10f3c98b7f6163f921cb87
lanrelawal0022 <lanrelawal2002@gmail.com>
2022-07-19 01:02:19 +0000

    list file

---
+++ PYTHONGURU/section4/list-combined.py
@@ -0,0 +1,30 @@
+list = [11, 33, 44,66, 788, 1]
+print(list[0:5])
+print(list[:3])
+print(list[2:])
+
+print("hello")
+print("hello")
+print("hello")
+
+list1 = [11, 33]
+list2 = [1, 9]
+list3 = list1 + list2
+print(list3)
+
+
+print("hello")
+print("hello")
+print("hello")
+
+list4 = [1, 2, 3, 4]
+list5 = list4 * 4
+print(list5)
+
+list1 = [11, 22, 44,16, 77, 98]
+print(22 in list1)
+print(22 not in list1)
+
+list22 = [1,2,3,4,5]
+for i in list22:
+    print(i)
---
+++ PYTHONGURU/section4/list-for-only.py
@@ -0,0 +1,3 @@
+list22 = ["hello"]
+for i in list22:
+    print(i, end="18")
